9f7d5a81551129fda7da65c08694a759427aeb49
Tautvilas <tautvilas@wix.com>
2013-10-06 18:24:11 +0300

    fourth experiment

---
+++ four/fragment.c
@@ -0,0 +1,13 @@
+precision mediump float;
+
+varying vec2 position;
+uniform float time;
+
+void main() {
+    float intensity = abs(0.5 - mod(time, 500.0) / 500.0);
+    gl_FragColor = vec4(
+            intensity * (1.0 - abs(abs(position.x) + abs(position.y))),
+            0.0,
+            0.0,
+            1.0);
+}
---
+++ four/vertex.c
@@ -0,0 +1,13 @@
+attribute vec2 a_position;
+varying vec2 position;
+uniform float scale;
+uniform float time;
+
+void main() {
+    float intensity = abs(0.5 - mod(time, 500.0) / 500.0) / 0.5;
+    gl_Position = vec4(
+            a_position.x * intensity,
+            a_position.y * intensity, 0, 1.0 * scale);
+    position = a_position;
+}
+
