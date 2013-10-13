8e01eedb08abc7821e1fcb00e3e1cbc8c0ef4a06
Tautvilas <tautvilas@wix.com>
2013-10-13 15:13:30 +0300

    x

--- five/vertex.c
+++ five/vertex.c
@@ -1,6 +1,8 @@
 attribute vec4 a_position;
 
 void main() {
-    gl_Position = a_position;
+    vec4 camera_pos = a_position + vec4(0.5, 0.5, 0, 0);
+    camera_pos.w = -a_position.z;
+    gl_Position = camera_pos;
 }
 
