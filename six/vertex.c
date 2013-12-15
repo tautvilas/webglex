2f30e5d2172bc37db14cf9043359a2de3bc96bd7
tautvilas <tautvilas@mediapark.lt>
2013-12-15 23:05:16 +0200

    buggy X spinning

--- six/vertex.c
+++ six/vertex.c
@@ -1,4 +1,4 @@
-precision mediump float;
+//precision mediump float;
 
 attribute vec4 a_position;
 attribute vec4 a_color;
@@ -11,8 +11,15 @@ uniform float zFar;
 uniform float frustumScale;
 
 void main() {
-    float scale = (mod(time, 1000.0)) / 1000.0;
-    vec4 cameraPos = (a_position * scale + vec4(0.0, 0.0, -2.0, 0.0));
+    //float scale = (mod(time, 1000.0)) / 1000.0;
+    float scale = 1.0;
+    float angle = 3.14 * (mod(time, 1000.0) / 1000.0);
+    vec4 cameraPos = a_position;
+    cameraPos.y = cameraPos.y * cos(angle) - cameraPos.z * sin(angle);
+    cameraPos.z = cameraPos.y * sin(angle) + cameraPos.z * cos(angle);
+
+    cameraPos = (cameraPos * scale + vec4(1, 1, -3.0, 0.0));
+
     vec4 clipPos;
 
     clipPos.xy = cameraPos.xy;
