a304652ecc43d690ef0114a518aa642638c67e8e
tautvilas <tautvilas@mediapark.lt>
2013-12-15 22:33:38 +0200

    Refactor, use unit cube

--- five/vertex.c
+++ five/vertex.c
@@ -7,23 +7,16 @@ uniform float zFar;
 uniform float frustumScale;
 
 void main() {
-    vec4 cameraPos = (a_position + vec4(0.3, 0.3, 0.0, 0.0));
+    vec4 cameraPos = (a_position * 0.5 + vec4(0.6, 0.6, -2.0, 0.0));
     vec4 clipPos;
 
-    /*
-    clipPos.xy = cameraPos.xy * 1.0;
+    clipPos.xy = cameraPos.xy;
 
     clipPos.z = cameraPos.z * (1.0 + 3.0) / (1.0 - 3.0);
     clipPos.z += 2.0 * 1.0 * 3.0 / (1.0 - 3.0);
 
-    clipPos.z = 0.0;
     clipPos.w = -cameraPos.z;
-    */
-
-    float zToDivideBy = (2.0 + -cameraPos.z) * 1.0;
-    cameraPos.xy = cameraPos.xy / zToDivideBy;
-    cameraPos.w = 1.0;
-    gl_Position = cameraPos;
+    gl_Position = clipPos;
 
     theColor = a_color;
 }
